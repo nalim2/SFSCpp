6353ac29e3162924ed89eabd936452bc5db3211d
chrizzly123 <24834128+chrizzly123@users.noreply.github.com>
2020-04-08 01:49:41 +0200

    improve detaching of attached jni threads

--- jnizmq/src/impl/JvmManager.cpp
+++ jnizmq/src/impl/JvmManager.cpp
@@ -7,9 +7,11 @@ namespace JvmStore {
 JNIEnv *JvmManager::attachThread() {
     JNIEnv *env;
     JvmStore::vm->AttachCurrentThread((void **) &env, nullptr);
+    struct DetachOnJniExit {
+        ~DetachOnJniExit() {
+            JvmStore::vm->DetachCurrentThread();
+        }
+    };
+    thread_local DetachOnJniExit detach;
     return env;
-}
-
-void JvmManager::detachThread() {
-    JvmStore::vm->DetachCurrentThread();
 }
--- jnizmq/src/impl/JvmManager.h
+++ jnizmq/src/impl/JvmManager.h
@@ -4,6 +4,7 @@
 #include <jni.h>
 #include <memory>
 #include <functional>
+#include <iostream>
 
 namespace JvmStore {
     extern JavaVM *vm;
@@ -11,16 +12,8 @@ namespace JvmStore {
 
 class JvmManager {
 public:
-
     static JNIEnv *attachThread();
-
-    static void detachThread();
-
-    static void detachThreadAdapter(void *) { detachThread(); }
 };
-
-thread_local static std::unique_ptr<void *, decltype(&JvmManager::detachThreadAdapter)>
-        detacher(nullptr, JvmManager::detachThreadAdapter);
 
 
 #endif //ZMQLOOPLIB_JVMMANAGER_H
